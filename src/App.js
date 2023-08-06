6ee208847c7e5b28fa2791494455a8347d03719f
Shehab Waleed <shehabwaleedd@gmail.com>
2023-08-06 13:01:56 +0400

    added favicons and meta description

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import ScrollUp from './components/scrollUp/ScrollUp';
