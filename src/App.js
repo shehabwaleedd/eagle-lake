aeafa742d12498cf4b901ac0d16acc36611eb05c
Shehab Waleed <shehabwaleedd@gmail.com>
2023-08-07 22:40:18 +0400

    fixed minor component's colors and position

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './App.css';
-
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import ScrollUp from './components/scrollUp/ScrollUp';
-import Navbar from './navbar/Navbar';
 import Home from './pages/home/Home';
-import RightSide from './pages/rightSide/RightSide';
+import { useState } from 'react';
 function App() {
   const location = useLocation();
+  const [navOpen, setNavOpen] = useState(false);
   return (
     <div className="App">
       <AnimatePresence mode='wait'>
         <Routes location={location} key={location.pathname} >
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<Home navOpen={navOpen} setNavOpen={setNavOpen}/>} />
         </Routes>
       </AnimatePresence>
       <ScrollUp />
