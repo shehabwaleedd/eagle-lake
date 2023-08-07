99d298fbf5ce16cd629de94e7952d46f3ed99d65
Shehab Waleed <shehabwaleedd@gmail.com>
2023-08-08 01:00:55 +0400

    finished book a table section design wise

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,16 +4,18 @@ import RightSide from '../rightSide/RightSide'
 import './Home.css'
 import Intro from './leftSide/Intro/Intro'
 import About from './leftSide/about/About'
+import Reservation from './leftSide/reservation/Reservation'
 
 
 
-const Home = ({navOpen, setNavOpen, isMobile, setIsMobile, setIsTablet, isTablet}) => {
+const Home = ({ navOpen, setNavOpen, isMobile, setIsMobile, setIsTablet, isTablet }) => {
     return (
         <div className='home'>
-            <Navbar navOpen={navOpen} setNavOpen={setNavOpen} isMobile={isMobile} setIsMobile={setIsMobile} setIsTablet={setIsTablet} isTablet={isTablet}/>
+            <Navbar navOpen={navOpen} setNavOpen={setNavOpen} isMobile={isMobile} setIsMobile={setIsMobile} setIsTablet={setIsTablet} isTablet={isTablet} />
             <div className="left__side">
                 <Intro />
                 <About />
+                <Reservation />
             </div>
             <div className="right__side">
                 <RightSide />
--- src/pages/home/leftSide/reservation/Reservation.jsx
+++ src/pages/home/leftSide/reservation/Reservation.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import './Reservation.css'
+import { Link } from 'react-router-dom'
 
 const Reservation = () => {
     return (
         <section className='reservation'>
             <div className="reservation__container">
                 <h1>Book A Table</h1>
+                <div className="reservation__table">
+                    <div className="reservation__table-left">
+                        <div className="reservation__table-left__container">
+                            <div className="reservation_vertical_dash"></div>
+                            <div className="reservation__table-left__container__input">
+                                <label htmlFor="name">Name:</label>
+                                <input type="text" name="name" id="name" placeholder='Enter your name' />
+                            </div>
+                            <div className="reservation__table-left__container__input">
+                                <label htmlFor="email">Email:</label>
+                                <input type="email" name="email" id="email" placeholder='Enter your email' />
+                            </div>
+                            <div className="reservation__table-left__container__input">
+                                <label htmlFor="phone">Phone:</label>
+                                <input type="text" name="phone" id="phone" placeholder='Enter your phone number' />
+                            </div>
+                            <div className="reservation__table-left__container__input">
+                                <label htmlFor="date">Date:</label>
+                                <input type="date" name="date" id="date" placeholder='Enter your date' />
+                            </div>
+                            <div className="reservation__table-left__container__input">
+                                <label htmlFor="time">Time:</label>
+                                <input type="time" name="time" id="time" placeholder='Enter your time' />
+                            </div>
+                            <div className="reservation__table-left__container__input">
+                                <label htmlFor="guests">Guests:</label>
+                                <input type="number" name="guests" id="guests" placeholder='Enter your guests' />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="send__button">
+                    <button>Send</button>b
+                </div>
+            </div>
+            <div className="reservation__footer">
+                <p>© Eagle Lake 2023</p>
+                <span>Developed & Designed by <Link to="https://shehabwaleedd.vercel.app/">Shehab</Link></span>
             </div>
         </section>
     )
