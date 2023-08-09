2daabd801a8786ea3c1af6efac0fd7286da50142
Shehab Waleed <shehabwaleedd@gmail.com>
2023-08-09 04:12:38 +0400

    resized the partners images for better performance in mobile devices.

--- src/pages/home/leftSide/about/aboutComponents/AboutUpper.jsx
+++ src/pages/home/leftSide/about/aboutComponents/AboutUpper.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import "./AboutComponents.css"
 import partner1 from "../../../../../assets/partner1.webp"
+import partner1_300px from "../../../../../assets/partner1_300.webp"
 import partner2 from "../../../../../assets/partner2.webp"
+import partner2_300px from "../../../../../assets/partner2_300.webp"
 
 const AboutUpper = () => {
     return (
@@ -11,8 +13,12 @@ const AboutUpper = () => {
                 <p>Eagle Lake Breakfast Coffee Shop" is a delightful haven nestled in the heart of Sri Lanka, offering a charming and inviting ambiance for all coffee and breakfast enthusiasts. Located by the tranquil Tinara River in Weligama, this picturesque café is a must-visit destination for locals and travelers alike.</p>
                 <div className="aboutUpper__lower">
                     <div className="aboutUpper__lower-imgs">
-                        <img src={partner1} alt="" />
-                        <img src={partner2} alt="" />
+                        <img src={partner1} alt=""
+                            srcSet={[`${partner1_300px}?w=300&format=webp 300w`]}
+                        />
+                        <img src={partner2} alt=""
+                            srcSet={[`${partner2_300px}?w=300&format=webp 300w`]}
+                        />
                     </div>
                     <div className="about__lower_tex">
                         <span>Our founders</span>
